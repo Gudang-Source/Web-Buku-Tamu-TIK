934d8f1bfd567e4c3ea65f798e0710cc69c6fb16
ick888 <isnedi.14116136@student.itera.ac.id>
2019-06-27 16:42:11 +0700

    Test

--- include/version.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!isset($_SESSION["mikhmon"])) {
-    header("Location:../admin.php?id=login");
-  } else {
-        $_SESSION["v"] = "3.14 05-09-2019";
-        //echo '<span style="display:none" id="ver">314</span>';
-    
-    }
