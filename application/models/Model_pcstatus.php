a427c162542c7f6e868ef1207c1adefc747db619
ick888 <isnedi.14116136@student.itera.ac.id>
2019-07-15 14:29:25 +0700

    Initiate pcstatus #2

--- application/models/Model_pcstatus.php
+++ application/models/Model_pcstatus.php
@@ -4,18 +4,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Model_pcstatus extends CI_model {
     public function pcstatus1(){
         $mikrotik = array("01", "02", "03");
-        $id = 1;
 		$this->db->where('id',$id);
 		$this->db->where('mac-address',$mac);
 		$query = $this->db->get('pc');
 		if($query->num_rows()>0)
 		{
-			foreach ($query->result() as $row)
+			foreach ($query->result() as $macactive)
 			{
-				$sess = array('Email'	=> $row->Email,
+				/* $sess = array('Email'	=> $row->Email,
 							  'password'	=> $row->password);
 				$this->session->set_userdata($sess);
-				redirect('BerandaMhs');
+				redirect('BerandaMhs'); */
+
+				if ($pc1 == 0) {
+					//PC Rusak
+				} else {
+					//PC Aktif atau Tidak aktif
+				}
+				
 			}
 		}
     }
